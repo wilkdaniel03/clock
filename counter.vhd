09960f20bd54ab2086775b1df148d13a7b97213b
Daniel Wilk <wilkdaniel03@gmail.com>
2025-11-22 15:43:37 +0100

    Adding enable input to counter

--- counter.vhd
+++ counter.vhd
@@ -2,7 +2,7 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity counter is
-	port(clk : in std_logic; rst : in std_logic := '0'; dout : out std_logic_vector(3 downto 0));
+	port(clk,en : in std_logic; rst : in std_logic := '0'; dout : out std_logic_vector(3 downto 0));
 end counter;
 
 architecture arch of counter is
@@ -10,7 +10,7 @@ architecture arch of counter is
 begin
 	dout <= current;
 process(clk,rst) begin
-	if rising_edge(clk) then
+	if rising_edge(clk) and en = '1' then
 		current(3) <= (current(3) and (not(current(2) and current(1) and current(0)))) or (not(current(3)) and current(2) and current(1) and current(0));
 		current(2) <= (not(current(2)) and current(1) and current(0)) or (current(2) and (current(1) nand current(0)));
 		current(1) <= current(1) xor current(0);
--- counter_tb.vhd
+++ counter_tb.vhd
@@ -7,14 +7,16 @@ end counter_tb;
 
 architecture arch of counter_tb is
 	signal clk : std_logic := '1';
+	signal en : std_logic := '1';
 	signal rst : std_logic := '0';
 	signal dout : std_logic_vector(3 downto 0) := "0000";
 begin
 	clk <= not clk after 500 ps;
+	en <= not en after 5 ns;
 	rst <= dout(3) and dout(0);
 	y <= dout;
-	uut : entity work.counter port map(clk => clk, rst => rst, dout => dout);
+	uut : entity work.counter port map(clk => clk, en => en, rst => rst, dout => dout);
 process begin
-	wait for 16 ns;
+	wait for 25 ns;
 end process;
 end arch;
