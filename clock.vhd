2f6809cb70fb7b1e2a16c6315c14a8618caf9ac7
Daniel Wilk <wilkdaniel03@gmail.com>
2025-11-23 00:03:16 +0100

    4 segment displays encoders

--- clock.vhd
+++ clock.vhd
@@ -2,28 +2,56 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity clock is
-	port(clk : in std_logic; seg11,seg12,seg13,seg14,seg15,seg16,seg17 : out std_logic; y : out std_logic);
+	port(clk,btn : in std_logic;
+	seg11,seg12,seg13,seg14,seg15,seg16,seg17,
+	seg21,seg22,seg23,seg24,seg25,seg26,seg27,
+	seg31,seg32,seg33,seg34,seg35,seg36,seg37,
+	seg41,seg42,seg43,seg44,seg45,seg46,seg47 : out std_logic; y : out std_logic);
 end clock;
 
 architecture arch of clock is
 	signal prescaler_out : std_logic_vector(6 downto 0) := "0000000";
-	signal clk_1hz_out : std_logic_vector(3 downto 0) := "0000";
-	signal clk_1hz_det_max : std_logic := '0';
+	signal clk_in : std_logic := '0';
+	signal clk_5_out : std_logic_vector(3 downto 0) := "0000";
+	signal clk_5_det_max : std_logic := '0';
 	signal counter_out : std_logic_vector(3 downto 0) := "0000";
 	signal counter_det_max : std_logic := '0';
-	signal seg_enc_out : std_logic_vector(6 downto 0) := "0000000";
+	signal seg_enc1_out : std_logic_vector(6 downto 0) := "0000000";
+	signal seg_enc2_out : std_logic_vector(6 downto 0) := "0000000";
+	signal seg_enc3_out : std_logic_vector(6 downto 0) := "0000000";
+	signal seg_enc4_out : std_logic_vector(6 downto 0) := "0000000";
 begin
 	prescaler : entity work.prescaler port map(clk => clk, y => prescaler_out);
-	counter_1hz : entity work.counter port map(clk => clk, en => prescaler_out(0), rst => clk_1hz_det_max, dout => clk_1hz_out);
-	clk_1hz_det_max <= clk_1hz_out(2) and clk_1hz_out(0);
-	counter : entity work.counter port map(clk => clk, en => clk_1hz_det_max, rst => counter_det_max, dout => counter_out);
-	seg_encoder : entity work.seg_encoder port map(din => counter_out, dout => seg_enc_out);
-	counter_det_max <= counter_out(3) and counter_out(1);
-	seg11 <= seg_enc_out(6);
-	seg12 <= seg_enc_out(5);
-	seg13 <= seg_enc_out(4);
-	seg14 <= seg_enc_out(3);
-	seg15 <= seg_enc_out(2);
-	seg16 <= seg_enc_out(1);
-	seg17 <= seg_enc_out(0);
+	clk_in <= prescaler_out(0) when btn = '1' else prescaler_out(2);
+	counter_5 : entity work.counter port map(clk => clk, en => clk_in, rst => clk_5_det_max, dout => clk_5_out);
+	clk_5_det_max <= clk_5_out(2) and clk_5_out(1);
+	segments : entity work.segments port map(clk => clk, en => clk_5_det_max, seg1 => seg_enc1_out, seg2 => seg_enc2_out, seg3 => seg_enc3_out, seg4 => seg_enc4_out);
+	seg11 <= seg_enc1_out(6);
+	seg12 <= seg_enc1_out(5);
+	seg13 <= seg_enc1_out(4);
+	seg14 <= seg_enc1_out(3);
+	seg15 <= seg_enc1_out(2);
+	seg16 <= seg_enc1_out(1);
+	seg17 <= seg_enc1_out(0);
+	seg21 <= seg_enc2_out(6);
+	seg22 <= seg_enc2_out(5);
+	seg23 <= seg_enc2_out(4);
+	seg24 <= seg_enc2_out(3);
+	seg25 <= seg_enc2_out(2);
+	seg26 <= seg_enc2_out(1);
+	seg27 <= seg_enc2_out(0);
+	seg31 <= seg_enc3_out(6);
+	seg32 <= seg_enc3_out(5);
+	seg33 <= seg_enc3_out(4);
+	seg34 <= seg_enc3_out(3);
+	seg35 <= seg_enc3_out(2);
+	seg36 <= seg_enc3_out(1);
+	seg37 <= seg_enc3_out(0);
+	seg41 <= seg_enc4_out(6);
+	seg42 <= seg_enc4_out(5);
+	seg43 <= seg_enc4_out(4);
+	seg44 <= seg_enc4_out(3);
+	seg45 <= seg_enc4_out(2);
+	seg46 <= seg_enc4_out(1);
+	seg47 <= seg_enc4_out(0);
 end arch;
---
+++ segments.vhd
@@ -0,0 +1,30 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity segments is
+	port(clk,en : in std_logic; seg1,seg2,seg3,seg4: out std_logic_vector(6 downto 0));
+end segments;
+
+architecture arch of segments is
+	signal seg_din_1 : std_logic_vector(3 downto 0) := "0000";
+	signal seg_din_det1_max : std_logic := '0';
+	signal seg_din_2 : std_logic_vector(3 downto 0) := "0000";
+	signal seg_din_det2_max : std_logic := '0';
+	signal seg_din_3 : std_logic_vector(3 downto 0) := "0000";
+	signal seg_din_det3_max : std_logic := '0';
+	signal seg_din_4 : std_logic_vector(3 downto 0) := "0000";
+	signal seg_din_det4_max : std_logic := '0';
+begin
+	counter_1 : entity work.counter port map(clk => clk, en => en, rst => seg_din_det1_max, dout => seg_din_1);
+	seg_din_det1_max <= seg_din_1(3) and seg_din_1(1);
+	seg_enc1 : entity work.seg_encoder port map(din => seg_din_1, dout => seg1);
+	counter_2 : entity work.counter port map(clk => clk, en => seg_din_det1_max, rst => seg_din_det2_max, dout => seg_din_2);
+	seg_din_det2_max <= seg_din_2(2) and seg_din_2(1);
+	seg_enc2 : entity work.seg_encoder port map(din => seg_din_2, dout => seg2);
+	counter_3 : entity work.counter port map(clk => clk, en => seg_din_det2_max, rst => seg_din_det3_max, dout => seg_din_3);
+	seg_din_det3_max <= (seg_din_3(3) and seg_din_3(1)) or (seg_din_4(1) and seg_din_3(2));
+	seg_enc3 : entity work.seg_encoder port map(din => seg_din_3, dout => seg3);
+	counter_4 : entity work.counter port map(clk => clk, en => seg_din_det3_max, rst => seg_din_det4_max, dout => seg_din_4);
+	seg_din_det4_max <= seg_din_4(1) and seg_din_3(2);
+	seg_enc4 : entity work.seg_encoder port map(din => seg_din_4, dout => seg4);
+end arch;
